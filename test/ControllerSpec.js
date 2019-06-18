e7d7af8947003b607722025c679f49c285d7b1cd
EmericHochart <gameovor@gmail.com>
2019-06-18 13:06:06 +0200

    Config jasmine, add 2 tests

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -135,10 +135,22 @@ describe('controller', function () {
 
 	it('should highlight "All" filter by default', function () {
 		// TODO: write test
+		
+		setUpModel([]);
+
+		subject.setView('');
+
+		expect(view.render).toHaveBeenCalledWith('setFilter', '');
+
 	});
 
 	it('should highlight "Active" filter when switching to active view', function () {
 		// TODO: write test
+		
+		setUpModel([]);		
+		subject.setView('#/active');
+
+		expect(view.render).toHaveBeenCalledWith('setFilter', 'active');
 	});
 
 	describe('toggle all', function () {
